753660a956305e845966f8c4a9c6ced9f5fc2beb
markziemann <mark.ziemann@gmail.com>
2019-05-29 15:30:33 +1000

    is now  argument

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COUNT=200
+COUNT=$1
 echo "$COUNT"
 
 formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa
@@ -8,4 +8,3 @@ formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.f
 msbar -sequence 5.fa -count $COUNT -point 4 -block 0 -codon 0 -outseq mysample_mutated.fa
 
 blast2 -m 8 -p blastn -e 0.001 -d Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa -i mysample_mutated.fa
-
