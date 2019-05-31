4363f53948d397813d4e4d7ef2a518387959aa30
parveenkumari96 <50190719+parveenkumari96@users.noreply.github.com>
2019-05-31 13:29:02 +1000

    Update description .R

--- description .R
+++ description .R
@@ -1,1 +1,3 @@
-
+in the R- studio the file gene_expression .tsv is downloaded by the code provided in assignment. then read in table command run and the gene expression numbers are recorded.
+The commands listed in the assignmnet report gives the mean, highest mean esxpressions,count for genes with average<10. 
+the pairing in R studio plot the data as pairs plot that represented the whole information of gene expression in a concise way.
